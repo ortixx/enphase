1ebd8bb164055931e62f9f8678d31be71e1c83f3
fmo <ferry@montenij.com>
2023-06-21 11:35:08 +0200

    0.0.3

--- custom_components/enphase_envoy/sensor.py
+++ custom_components/enphase_envoy/sensor.py
@@ -341,7 +341,7 @@ class EnvoyInverterEntity(CoordinatorEntity, SensorEntity):
 
         self.async_on_remove(
             async_track_state_change_event(
-                self.hass, self._sensor_source.entity_id, calc_change
+                self.hass, calc_change
             )
         )
 
